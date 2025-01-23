2f02e66bc9afff0fae4e900c30bc0080963afc39
davorradic <45642322+emolionl@users.noreply.github.com>
2025-01-23 18:35:09 +0100

    return session_id
    every record should return id after record is inserted

--- py/main.py
+++ py/main.py
@@ -153,8 +153,12 @@ def session():
         if new_session is None:
             return jsonify({'error': 'No active session found'}), 404
 
-        # Insert the Session object into the database using aetherOneDB
-        aetherOneDB.insert_session(new_session)
+        # Insert the Session object into the database using aetherOneDB and get the user_id
+        session_id = aetherOneDB.insert_session(new_session)
+
+        # Add the user_id to the session object or response data
+        session_data = new_session.to_dict()
+        session_data['user_id'] = session_id
 
         response_data = json.dumps(new_session.to_dict(), ensure_ascii=False)
         return Response(response_data, content_type='application/json; charset=utf-8')
